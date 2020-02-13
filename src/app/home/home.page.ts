cebdb8c2046e58b61f92251c8610b51df2fb8209
Diego Arantes <dyegoh16@gmail.com>
2020-02-13 15:31:33 -0300

    Implementado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+import { File } from '@ionic-native/File/ngx';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { Platform } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private file: File,
+    private ft: FileTransfer,
+    private fileOpener: FileOpener,
+    private document: DocumentViewer) {}
 
+  openLocalPdf() {
+    let filePath = this.file.applicationDirectory + 'www/assets';
+
+    if (this.platform.is('android')) {
+      const fakeName = Date.now();
+      this.file.copyFile(filePath, '5-tools.pdf', this.file.dataDirectory, `${fakeName}.pdf`).then(result => {
+        this.fileOpener.open(result.nativeURL, 'application/pdf');
+      });
+
+    } else {
+      const options: DocumentViewerOptions = {
+        title: 'My PDF'
+      };
+      this.document.viewDocument(`${filePath}/5-tools.pdf`, 'application/pdf', options);
+    }
+  }
+
+  downloadAndOpenPdf() {
+    let downloadUrl = 'https://ws.autoscore.com.br/check/52421786-7e9c-4247-83ff-18e8d7bd5cca';
+    let path = this.file.dataDirectory;
+    const transfer = this.ft.create();
+
+    transfer.download(downloadUrl, `${path}myfile.pdf`).then(entry => {
+      let url = entry.toURL();
+      if (this.platform.is('ios')) {
+        this.document.viewDocument(url, 'application/pdf', {});
+
+      } else {
+        this.fileOpener.open(url, 'application/pdf');
+      }
+
+    });
+  }
 }
